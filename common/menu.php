d1bc7af1ce38a359006040de840ffad3c2518e6d
gbajekal <gautam_bajekal@hotmail.com>
2017-04-13 11:13:43 +0530

    - Removed Shopping cart from the link

--- common/menu.php
+++ common/menu.php
@@ -9,7 +9,7 @@ echo '<nav>';
 echo '<ul>';
 //echo '<li><a href="../registration/registration.php">Registration</a></li>';
 echo '<li><a href="../record/record.php">Recording</a></li>';
-echo '<li><a href="../shoppingcart/shoppingcart.php">Shopping Cart</a></li>';
+//echo '<li><a href="../shoppingcart/shoppingcart.php">Shopping Cart</a></li>';
 echo '</ul>';
 echo '</nav>';
 
